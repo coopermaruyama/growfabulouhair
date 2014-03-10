04acfcb33e16153907f23d3f16eedecf3e1e1d6c
Alain Jacomet <alainjacomet@outlook.com>
2014-03-10 09:26:44 -0400

    Checkout design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,4 +7,39 @@
 
     });
 
+
+    /***
+     * Graph
+     */
+
+    var chart = c3.generate({
+        size: {
+            height: 358,
+            width: 425
+        },
+        data: {
+            columns: [
+            ]
+        },
+        axis: {
+            y: {
+                label: 'Growth Score'
+            },
+            x: {
+                type: 'categorized',
+                categories: ['BT', '4W', '8W', '9W']
+            }
+        }
+    });
+    $('#chart').one('inview', function(event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            chart.load({
+                columns: [
+                        ['A', 0, 0.1, 0.1, 0.1],
+                        ['B', 0, 0.4, 0.5, 0.6],
+                        ['C', 0, 1.3, 2.0, 2.3]
+                    ]
+                });
+        }
+    });
 })(jQuery);
